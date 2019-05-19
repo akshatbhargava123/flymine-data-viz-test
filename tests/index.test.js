64ade978e51b39e63e9cc06ebcc65b0ef5e12f09
akshat <akshatbhargavadeepa@gmail.com>
2019-05-19 09:21:53 +0530

    updated test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,10 @@
-const main = require('../src').main;
+const main = require('../src').default;
 
 // Example
 describe('main', () => {
 	test('should throw error when called with wrong signature', () => {
 		expect(() => {
-      // testing with all falsy values
+			// testing with all falsy values
 			main('', 0, null, undefined, []);
 		}).toThrowError('Call main with correct signature');
 	});
